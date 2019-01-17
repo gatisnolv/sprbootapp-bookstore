e20da37fdbd3c9c0f55fed02a89076d54a29c762
gatis.liepinlauskis <gatisnolv@gmail.com>
2019-01-17 14:25:38 +0200

    implement LoadDatabase to preload some books

---
+++ src/main/java/bookstore/LoadDatabase.java
@@ -0,0 +1,24 @@
+package bookstore;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Slf4j
+public class LoadDatabase {
+
+    @Bean
+    CommandLineRunner initDatabase(BookRepository repository) {
+        return args -> {
+            repository.save(new Book("1984", "George Orwell", "Secker & Warburg", "1949-06-08"));
+            repository.save(new Book("To Kill a Mockingbird", "Harper Lee", "J. B. Lippincott & Co.", "1960-11-07"));
+            repository.save(new Book("Animal Farm", "George Orwell", "Secker & Warburg", "1945-08-17"));
+
+            repository.findAll().forEach(book -> {
+                log.info("Preloading " + book);
+            });
+        };
+    }
+}
